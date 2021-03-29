23ea5b133c47bab680d2dcdeb26ae17f15de3b99
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-03-29 15:35:00 +1300

    change link string based on pokemon, not map(index)

--- src/components/Menu/Options/PokemonList/PokemonListRenderer.tsx
+++ src/components/Menu/Options/PokemonList/PokemonListRenderer.tsx
@@ -14,12 +14,17 @@ interface Props {
 
 export const PokemonListRenderer: React.FC<Props> = (props) => {
     return (
+        <>
         <ul>
             <Link to="/pokemon/1">To pokemon</Link>
             {props.pokeList.map((pokemon, index) => {
-                let link = "/pokemon/" + (index + 1)
-            return <li><Link to={link}><p>{capitalize(pokemon.name)}</p></Link></li>
+                
+                // let link = "/pokemon/" + (index + 1)
+                //pokemon.url gives the api address for that pokemon, this removes everything but /pokemon/number
+                let link = pokemon.url.replace("https://pokeapi.co/api/v2", "")
+            return <li key={index}><Link to={link}><p>{capitalize(pokemon.name)}</p></Link></li>
             })}
         </ul>
+        </>
     )
 }
