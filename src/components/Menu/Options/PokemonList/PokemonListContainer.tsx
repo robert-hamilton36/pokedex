2785eeef77a7d38bbab35cdc5f2f084d53dfb00b
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-03-29 15:37:20 +1300

    offset, limit variables for new buttons and options

--- src/components/Menu/Options/PokemonList/PokemonListContainer.tsx
+++ src/components/Menu/Options/PokemonList/PokemonListContainer.tsx
@@ -1,18 +1,38 @@
 import React,{ useEffect, useState }  from 'react'
 
 import { getPokemonList } from '../../../../apis/pokemon'
+import { PokemonListButtons } from './PokemonListButtons'
+import { PokemonListLimitOptions } from './PokemonListLimitOptions'
 import { PokemonListRenderer } from './PokemonListRenderer'
 
+interface Pokemon {
+    name: string
+    url:string
+}
 
 export const PokemonListContainer = () => {
-    const [pokeList, setPokeList] = useState([])
+    const [pokeList, setPokeList] = useState<Array<Pokemon>>([])
+    const [offset, setOffset] = useState<number>(0)
+    const [limit, setLimit] = useState<number>(50)
+
+    const childSetOffset = (value: number) => {
+        setOffset(value)
+    }
+
+    const childSetLimit = (value:number) => {
+        setLimit(value)
+    }
 
     useEffect (()=> {
-        getPokemonList()
+        getPokemonList(limit, offset)
         .then( results => setPokeList(results) )
-    }, [])
+    }, [offset, limit])
     
     return (
+        <>
+        <PokemonListLimitOptions limit={limit} setLimit={childSetLimit}/>
+        <PokemonListButtons limit={limit} offset={offset} setOffset={childSetOffset}/>
         <PokemonListRenderer pokeList={pokeList}/>
+        </>
     )
 }
