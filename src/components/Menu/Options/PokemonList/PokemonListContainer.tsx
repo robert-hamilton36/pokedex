8d2f2da5749ac8f91c57ea1bfa6c0f50dce2c835
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-03-29 14:26:17 +1300

    fix memory leak infinite render

--- src/components/Menu/Options/PokemonList/PokemonListContainer.tsx
+++ src/components/Menu/Options/PokemonList/PokemonListContainer.tsx
@@ -10,7 +10,7 @@ export const PokemonListContainer = () => {
     useEffect (()=> {
         getPokemonList()
         .then( results => setPokeList(results) )
-    })
+    }, [])
     
     return (
         <PokemonListRenderer pokeList={pokeList}/>
