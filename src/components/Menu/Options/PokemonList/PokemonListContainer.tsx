f0da20ee6375e3b74f8dc00d9b21851593954a1b
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-03-29 14:00:04 +1300

    move pokemonList to new folder

--- src/components/Menu/Options/PokemonList/PokemonListContainer.tsx
+++ src/components/Menu/Options/PokemonList/PokemonListContainer.tsx
@@ -1,6 +1,6 @@
 import React,{ useEffect, useState }  from 'react'
 
-import { getPokemonList } from '../../apis/pokemon'
+import { getPokemonList } from '../../../../apis/pokemon'
 import { PokemonListRenderer } from './PokemonListRenderer'
 
 
--- src/components/Menu/Options/PokemonList/PokemonListRenderer.tsx
+++ src/components/Menu/Options/PokemonList/PokemonListRenderer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { capitalize } from '../../helperFunctions'
+import { capitalize } from '../../../../helperFunctions'
+
 
 interface Pokemon {
     name: string
@@ -13,12 +14,12 @@ interface Props {
 
 export const PokemonListRenderer: React.FC<Props> = (props) => {
     return (
-        <div>
+        <ul>
             <Link to="/pokemon/1">To pokemon</Link>
             {props.pokeList.map((pokemon, index) => {
                 let link = "/pokemon/" + (index + 1)
-            return <Link to={link}><h1>{capitalize(pokemon.name)}</h1></Link>
+            return <li><Link to={link}><p>{capitalize(pokemon.name)}</p></Link></li>
             })}
-        </div>
+        </ul>
     )
 }
