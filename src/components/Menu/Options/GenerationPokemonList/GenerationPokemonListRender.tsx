a1791a0553a7aacecf15e8b15d13fa8e93deb008
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-03-31 17:38:23 +1300

    add files for generational view, before refactor

---
+++ src/components/Menu/Options/GenerationPokemonList/GenerationPokemonListRender.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { capitalize } from '../../../../helperFunctions'
+
+//TODO refactor the options generator into a single file
+
+interface Props {
+    pokedexList: Array<GenerationObject>
+    selectedPokedexVersion: number
+    setSelectedPokedexVersion: (number:number) => void
+}
+
+interface GenerationObject{
+    name: string
+    url:string
+}
+
+export const PokedexListRender: React.FC<Props> = (props) => {
+
+    return (
+        <select value={props.selectedPokedexVersion} onChange={(event) => props.setSelectedPokedexVersion(parseInt(event.target.value))}>
+            {props.pokedexList.map((pokedex) => {
+                let value:string = pokedex.url.replace("https://pokeapi.co/api/v2/pokedex/", "").replace("/", "")
+                let name:string = capitalize(pokedex.name.replace("original-", ""))
+                return <option key={pokedex.name} value={value}>{name}</option>
+            })}
+        </select>
+    )
+}
